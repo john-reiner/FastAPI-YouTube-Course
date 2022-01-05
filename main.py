257c0917f8da06238557ae84d7835abd8ea2be2d
john-reiner <john.reiner@datadoghq.com>
2022-01-05 15:22:04 -0500

    get and post requests

--- main.py
+++ main.py
@@ -1,29 +1,66 @@
+from typing import Optional
 from fastapi import FastAPI
-from datetime import date
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    payload =  {
+@app.get('/blog')
+def index(limit: int, authenticated: bool = False):
+    
+    if authenticated:    
+        if limit > 1:
+            payload =  {
+                "data" : f"Here is {limit} blogs"
+            }
+        else:
+            payload =  {
+                "data" : f"Here is the {limit} blog requested."
+            }
+        return payload
+    else:
+        return "Please get authenticated first!"
+
+@app.get('/blog/unpublished')
+def unpublished():
+    payload = {
         "data" : {
-            "name" : "John Reiner"
+            "unpublished_blogs" : [{"title" : "upb 1"}]
+        }
+    }
+    return payload
+
+@app.get('/blog/{id}')
+def show(id: int):
+    payload = {
+        "data" : id
+    }
+    return payload
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    payload = {
+        "data": {
+            "1"
+        }
+    }
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    
+    payload = {
+        "data" : {
+            "message" : f"{request.title} created!",
+            "status" : 201
         }
     }
     
-    # return f"Hello "+ payload['data']['name'] +"!"
     return payload
-
-@app.get('/about')
-def about():
-    today = date.today()
-    payload = {
-        "data" : {
-            "name" : "John Reiner",
-            "created" : "2022-01-05",
-            "today's date" : "Today's date: " + str(today)
-        }
-    }
-    return payload
-
+    
